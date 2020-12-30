2e466c26a52d8b0cd5ddb0eb4132d6823af43701
Jonas Schäfer <j.wielicki@sotecware.net>
2020-12-30 14:06:02 +0100

    Update update script to use new scripts

--- update-daily-non-idempotent.sh
+++ update-daily-non-idempotent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'DANGER: Running this twice on the same data set corrupts the metrics!' >&2
-./rki-deaths-incr-to-influx.py --no-send rki/cases.csv -o rki/deaths.csv
-./rki-deaths-incr-to-influx.py rki/deaths.csv -q
+./rki-deaths-cases-to-csv.py rki/cases.csv -o rki/deaths.csv
+./rki-deaths-to-influx.py rki/deaths.csv
