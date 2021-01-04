98e304e69614271b4db886654fa93ca3238d8f85
Jonas Schäfer <j.wielicki@sotecware.net>
2021-01-04 11:23:09 +0100

    Automatically archive and compress cases data

--- download-rki.sh
+++ download-rki.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 data="https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
 districts="https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0.csv"
 outdir="$(pwd)/rki"
+outfile="$outdir/cases-$(date +%Y-%m-%d).csv"
+outfile_gz="$outfile.gz"
+currentfile="$outdir/cases.csv.gz"
 district_path="$outdir/districts.csv"
 mkdir -p "$outdir"
 if [ ! -f "$district_path" ]; then
@@ -10,4 +13,20 @@ if [ ! -f "$district_path" ]; then
 else
     printf 'note: skipping download of district information as it is already present\n'
 fi
-wget -O "$outdir/cases.csv" "$data"
+if [ ! -f "$outfile" ] && [ ! -f "$outfile_gz" ]; then
+    tmpout="$(mktemp -p "$outdir")"
+    function cleanup() {
+        rm -f "$tmpout"
+    }
+    trap cleanup EXIT
+    wget -O "$tmpout" "$data"
+    mv "$tmpout" "$outfile"
+else
+    printf 'note: skipping download of cases data since it is already present for today\n'
+fi
+if [ ! -f "$outfile_gz" ]; then
+    gzip -9 "$outfile"
+else
+    printf 'note: skipping compression of cases data since it is already present for today\n'
+fi
+ln -sf "$(basename "$outfile")" "$currentfile"
