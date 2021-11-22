f713e3c96e332051b282a5d4b823f4b4b49dd480
Jonas Schäfer <j.wielicki@sotecware.net>
2021-11-22 17:53:41 +0100

    Avoid buffering uncompressed files on disk
    There's no reason not to pipe them to gzip right away. Goes easier on my
    poor spinning rust (and the compacted sand is obviously also happy with
    fewer writes).

--- download-versioned.sh
+++ download-versioned.sh
@@ -3,24 +3,18 @@ set -euo pipefail
 data="$1"
 outdir="$2"
 prefix="$3"
-outfile="$outdir/$prefix-$(date +%Y-%m-%d).csv"
-outfile_gz="$outfile.gz"
+outfile="$outdir/$prefix-$(date +%Y-%m-%d).csv.gz"
 currentfile="$outdir/$prefix.csv.gz"
 mkdir -p "$outdir"
-if [ ! -f "$outfile" ] && [ ! -f "$outfile_gz" ]; then
+if [ ! -f "$outfile" ]; then
     tmpout="$(mktemp -p "$outdir")"
     function cleanup() {
         rm -f "$tmpout"
     }
     trap cleanup EXIT
-    wget -O "$tmpout" "$data"
+    wget -O- "$data" | gzip -c9 > "$tmpout"
     mv "$tmpout" "$outfile"
 else
     printf 'note: skipping download of data since it is already present for today\n'
 fi
-if [ ! -f "$outfile_gz" ]; then
-    gzip -9 "$outfile"
-else
-    printf 'note: skipping compression of data since it is already present for today\n'
-fi
-ln -sf "$(basename "$outfile_gz")" "$currentfile"
+ln -sf "$(basename "$outfile")" "$currentfile"
