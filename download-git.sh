02d1bf302a1996a0a4c5cdcb68015eb5ae0fc7a9
Jonas Schäfer <j.wielicki@sotecware.net>
2022-07-23 14:11:14 +0200

    Remove submodules
    They are way too space expensive.

--- download-git.sh
+++ download-git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-git submodule foreach git checkout master
-git submodule foreach git reset --hard origin/master
-git submodule foreach git clean -fxd
-git submodule foreach git pull
+mkdir -p rki/vaccination-git/
+wget -O rki/vaccination-git/Aktuell_Deutschland_Landkreise_COVID-19-Impfungen.csv https://github.com/robert-koch-institut/COVID-19-Impfungen_in_Deutschland/raw/master/Aktuell_Deutschland_Landkreise_COVID-19-Impfungen.csv
+mkdir -p rki/hospitalization-git/
+wget -O rki/hospitalization-git/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv https://github.com/robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland/raw/master/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv
